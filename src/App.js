9903a89fdf7c509691507e619a775abaf16ff869
hsuan9008 <b0829032@cgu.edu.tw>
2021-05-02 21:57:22 +0800

    hw3

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HW3 from './hw3';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      { HW3()}
+    </div>
+    </div>
+  );
+}
+
+export default App;
